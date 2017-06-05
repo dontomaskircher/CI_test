2acfbd6d231f68d516ffe49d1c4194ba3021c8cc
Tomas Kircher <tomas.kircher@mrshoebox.com>
2017-06-05 17:01:05 +0200

    some test code

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-git remote add origin https://github.com/dontomaskircher/CI_test
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
